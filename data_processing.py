91c58ccb3be7c87e9250bf79574eba6edbb61ebb
Yaman Habip <56274952+Yaman-Habip@users.noreply.github.com>
2023-05-30 00:57:51 -0400

    d

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,3 @@
-in_model = []
+in_model = [[0],[1]]
 out_model = []
 max_val = max([len(i) for i in in_model])
---
+++ scrape.py
@@ -0,0 +1,40 @@
+import requests
+from artists import rock_artists
+from bs4 import BeautifulSoup
+import csv
+
+# Generate a list of songs based off of rock artists
+def get_songs(artist): # Expects an artist, returns e-chords urls of songs of that artist as a list
+
+  # Creating a BeautifulSoup obejct for a given artists
+  response = requests.get("https://www.e-chords.com/" + artist.replace(" ", "-"))
+  html_content = response.text
+  soup = BeautifulSoup(html_content, 'html.parser')
+
+  # The songs are in <a></a> tags with class 'ta'
+  song_links = soup.find_all('a', class_='ta')
+
+  # Adding the href of the a tags to a list and returning it
+  return [i["href"] for i in song_links]
+
+# List of the songs we want to scrape
+urls = []
+for i in rock_artists:
+  urls.extend(get_songs(i))
+
+def get_chords(url): # Expects an e-chords url, returns chords of that song as a list
+  # Creating a BeautifulSoup obejct for a given song
+  response = requests.get(url)
+  html_content = response.text
+  soup = BeautifulSoup(html_content, 'html.parser')
+
+  # The chords are in <u></u> tags
+  u_tags = soup.find_all('u')
+
+  # Adding the content of the u tags to a list and returning it
+  return [i.text for i in u_tags]
+
+with open("master_chords.csv", 'a', newline='') as file:
+  writer = csv.writer(file)
+  for i in urls:
+    writer.writerow(get_chords(i))

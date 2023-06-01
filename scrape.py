7e5689168e247179b053e0f41f029c7f82e31325
Yaman Habip <56274952+Yaman-Habip@users.noreply.github.com>
2023-06-01 18:09:34 -0400

    More pushes

--- scrape.py
+++ scrape.py
@@ -20,7 +20,7 @@ def get_songs(artist): # Expects an artist, returns e-chords urls of songs of th
 # List of the songs we want to scrape
 urls = []
 for i in rock_artists:
-  urls.extend(get_songs(i))
+  urls.extend(list(set(get_songs(i))))
 
 def get_chords(url): # Expects an e-chords url, returns chords of that song as a list
   # Creating a BeautifulSoup obejct for a given song
@@ -34,7 +34,10 @@ def get_chords(url): # Expects an e-chords url, returns chords of that song as a
   # Adding the content of the u tags to a list and returning it
   return [i.text for i in u_tags]
 
-with open("master_chords.csv", 'a', newline='') as file:
+counter = 0
+with open("master_chords.csv", 'w', newline='') as file:
   writer = csv.writer(file)
   for i in urls:
+    counter += 1
+    print(counter, i)
     writer.writerow(get_chords(i))
